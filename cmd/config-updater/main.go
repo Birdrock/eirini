3e367ab03bd448c7f67bb7e20cf428f86c0eecd5
Georgi Sabev <georgethebeatle@gmail.com>
2020-05-29 17:13:06 +0000

    Replicate secret on startup of the config-updater
    This way the registry secret that is initially configured in eirini's
    configmap will be replicated in the app namespace so that it is
    available before it is updated for the first time.
    
    If the secret is not there the config-updater logs a warning but does
    not fail. This way if someone updates the value in the configmap, the
    updater will replicate the secret.
    
    [#173013466]

--- cmd/config-updater/main.go
+++ cmd/config-updater/main.go
@@ -10,13 +10,19 @@ import (
 	"code.cloudfoundry.org/eirini/k8s"
 	"code.cloudfoundry.org/lager"
 	"github.com/jessevdk/go-flags"
+	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog"
+)
+
+const (
+	eiriniConfigmapLabelSelector = "cloudfoundry.org/owner=eirini"
 )
 
 type options struct {
@@ -48,12 +54,28 @@ func main() {
 		},
 	})
 
+	configmaps, err := clientset.CoreV1().ConfigMaps("").List(metav1.ListOptions{
+		LabelSelector: eiriniConfigmapLabelSelector,
+	})
+	cmdcommons.ExitIfError(err)
+
+	if len(configmaps.Items) == 0 {
+		panic("eirini config not found")
+	}
+
+	eiriniNs := configmaps.Items[0].Namespace
+	if err := replicateSecret(clientset,
+		eiriniNs, cfg.Properties.RegistrySecretName,
+		cfg.Properties.Namespace, k8s.ImagePullSecretName); err != nil {
+		klog.Warningf("could not replicate app registry secret: %s", err.Error())
+	}
+
 	cancel := make(<-chan struct{})
 	informer.Run(cancel)
 }
 
 func tweakListOpts(opts *metav1.ListOptions) {
-	opts.LabelSelector = "cloudfoundry.org/owner=eirini"
+	opts.LabelSelector = eiriniConfigmapLabelSelector
 }
 
 func updateConfig(clientset kubernetes.Interface, oldConfig, updatedConfig *corev1.ConfigMap) {
@@ -61,26 +83,46 @@ func updateConfig(clientset kubernetes.Interface, oldConfig, updatedConfig *core
 	updatedCfg := unmarshalConfig([]byte(updatedConfig.Data["opi.yml"]))
 
 	if updatedCfg.Properties.RegistrySecretName != oldCfg.Properties.RegistrySecretName {
-		sourceSecretsClient := clientset.CoreV1().Secrets(updatedConfig.Namespace)
-		destSecretsClient := clientset.CoreV1().Secrets(updatedCfg.Properties.Namespace)
+		srcNs := updatedConfig.Namespace
+		srcSecret := updatedCfg.Properties.RegistrySecretName
+		dstNs := updatedCfg.Properties.Namespace
 
-		secret, err := sourceSecretsClient.Get(updatedCfg.Properties.RegistrySecretName, metav1.GetOptions{})
-		if err != nil {
-			klog.Errorf("failed to get registry secret: %s", err)
-			return
-		}
-
-		registrySecret := corev1.Secret{}
-		registrySecret.Name = k8s.ImagePullSecretName
-		registrySecret.Data = secret.Data
-		registrySecret.Type = secret.Type
-		registrySecret.Namespace = updatedCfg.Properties.Namespace
-		if _, err := destSecretsClient.Create(&registrySecret); err != nil {
-			klog.Errorf("failed to create registry secret: %s", err)
+		if err := replicateSecret(clientset, srcNs, srcSecret, dstNs, k8s.ImagePullSecretName); err != nil {
+			klog.Errorf("failed to replicate registry secret: %s/%s to %s/%s: %s", srcNs, srcSecret, dstNs, k8s.ImagePullSecretName, err)
 			return
 		}
 	}
+}
 
+func replicateSecret(clientset kubernetes.Interface, sourceNamespace, sourceSecretName, destNamespace, destSecretName string) error {
+	sourceSecretsClient := clientset.CoreV1().Secrets(sourceNamespace)
+	destSecretsClient := clientset.CoreV1().Secrets(destNamespace)
+
+	sourceSecret, err := sourceSecretsClient.Get(sourceSecretName, metav1.GetOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "replicate secret: failed to get source secret: %s/%s", sourceNamespace, sourceSecretName)
+	}
+
+	destSecret := corev1.Secret{}
+	destSecret.Name = k8s.ImagePullSecretName
+	destSecret.Data = sourceSecret.Data
+	destSecret.Type = sourceSecret.Type
+	destSecret.Namespace = destNamespace
+	if _, err := destSecretsClient.Create(&destSecret); err != nil {
+		if k8s_errors.IsAlreadyExists(err) {
+			existingSecret, err := destSecretsClient.Get(destSecretName, metav1.GetOptions{})
+			if err != nil {
+				return errors.Wrapf(err, "replicate secret: failed to get existing destination secret: %s/%s", sourceNamespace, sourceSecretName)
+			}
+			existingSecret.Data = sourceSecret.Data
+			if _, err := destSecretsClient.Update(existingSecret); err != nil {
+				return err
+			}
+			return nil
+		}
+		return errors.Wrapf(err, "replicate secret: failed to create new destination secret: %s/%s", destNamespace, destSecretName)
+	}
+	return nil
 }
 
 func loadConfigFromFile(path string) *eirini.Config {
